c53624779c96ee876613890479ff1816a91c4457
Machaelmus <michaeljtufo@gmail.com>
2021-07-24 15:51:09 -0400

    Navbar rendering started

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -1,38 +1,60 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from '../auth/LogoutButton';
+import Splash from '../Splash/Splash';
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
-  );
+  const sessionUser = useSelector(state => state.session.user);
+    if(sessionUser) {
+        return (
+          <nav>
+            <ul>
+              <li>
+                <NavLink to='/' exact={true} activeClassName='active'>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                  Sign Up
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/users' exact={true} activeClassName='active'>
+                  Users
+                </NavLink>
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+            </ul>
+          </nav>
+        );
+    } else {
+      return (
+        <nav>
+            <ul>
+              <li>
+                <NavLink to='/' exact={true} activeClassName='active'>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                  Login
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+      )
+    }
 }
 
 export default NavBar;

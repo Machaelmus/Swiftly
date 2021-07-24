443d177f095ea424ef1f1b7bf374eaa70710b43f
Machaelmus <michaeljtufo@gmail.com>
2021-07-24 16:39:53 -0400

    Background image added to the splash page

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -4,6 +4,7 @@ import styles from './Splash.module.css';
 const Splash = () => {
     return (
         <>
+        <div className={styles.backgroundImage}>
             <div className={styles.splashContainer}>
                 <h1 className={styles.splashHeading}>The power to connect with <span className={styles.splashPurpleWord}>anyone</span> in the world</h1>
                 <p className={styles.splashJoin}>Join today</p>
@@ -12,6 +13,7 @@ const Splash = () => {
                 <button className={styles.splashLogin1}>Log in</button>
                 <button className={styles.splashSignup}>Sign up</button>
             </div>
+        </div>
         </>
     )
 }

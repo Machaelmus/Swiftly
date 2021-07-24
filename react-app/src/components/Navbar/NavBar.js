455afe619941e58a0d4b9a321af397c908fbe05a
Machaelmus <michaeljtufo@gmail.com>
2021-07-24 15:56:42 -0400

    Navbar classes added and rendering finished for now

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -3,35 +3,30 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import LogoutButton from '../auth/LogoutButton';
-import Splash from '../Splash/Splash';
+import styles from './Navbar.module.css';
 
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user);
     if(sessionUser) {
         return (
-          <nav>
-            <ul>
-              <li>
+          <nav className={styles.navContainer}>
+            <ul className={styles.navUl}>
+              <li className={styles.navHome}>
                 <NavLink to='/' exact={true} activeClassName='active'>
                   Home
                 </NavLink>
               </li>
-              <li>
+              <li className={styles.navLogin}>
                 <NavLink to='/login' exact={true} activeClassName='active'>
                   Login
                 </NavLink>
               </li>
-              <li>
+              <li className={styles.navSignup}>
                 <NavLink to='/sign-up' exact={true} activeClassName='active'>
                   Sign Up
                 </NavLink>
               </li>
-              <li>
-                <NavLink to='/users' exact={true} activeClassName='active'>
-                  Users
-                </NavLink>
-              </li>
-              <li>
+              <li className={styles.navLogout}>
                 <LogoutButton />
               </li>
             </ul>
@@ -39,16 +34,16 @@ const NavBar = () => {
         );
     } else {
       return (
-        <nav>
-            <ul>
-              <li>
-                <NavLink to='/' exact={true} activeClassName='active'>
-                  Home
+        <nav className={styles.navSplashContainer}>
+            <ul className={styles.navSplashUl}>
+              <li className={styles.navSplashHome}>
+                <NavLink className={styles.navSplashHomeLink} to='/' exact={true} activeClassName='active'>
+                  {/* Image of the cheetah goes here */}
                 </NavLink>
               </li>
-              <li>
-                <NavLink to='/login' exact={true} activeClassName='active'>
-                  Login
+              <li className={styles.navSplashLogin}>
+                <NavLink className={styles.navSplashLoginLink} to='/login' exact={true} activeClassName='active'>
+                  <button className={styles.navSplashLoginButton}>Log in</button>
                 </NavLink>
               </li>
             </ul>

88ce3d337011557fc9a5538adea8ca816797f3b7
Machaelmus <michaeljtufo@gmail.com>
2021-07-24 20:25:05 -0400

    Login form positioned

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import styles from './Login.module.css';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,25 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className={styles.loginPageBackground}>
+      <div className={styles.loginContainer}>
+        <h1>Log in</h1>
+        <form className={styles.loginForm} onSubmit={onLogin}>
+          <div className={styles.loginErrorsContainer}>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            <input className={styles.loginEmail} name='email' type='text' placeholder='Email' value={email} onChange={updateEmail} />
+          </div>
+          <div>
+            <input className={styles.loginPassword} name='password' type='password' placeholder='Password' value={password} onChange={updatePassword} />
+          </div>
+          <button className={styles.loginSubmit} type='submit'>Login</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 

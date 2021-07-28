9dfa18886527cc677d20716268e9c49ee707f9b8
Machaelmus <michaeljtufo@gmail.com>
2021-07-28 18:48:24 -0400

    Single post page component created

---
+++ react-app/src/components/SinglePostPage/SinglePostPage.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import { useSelector } from 'react-redux';
+
+const SinglePostPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SinglePostPage;

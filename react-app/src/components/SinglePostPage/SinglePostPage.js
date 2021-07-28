52aaacc975adbac7a4bf9f34f1a9f759e93a9372
Machaelmus <michaeljtufo@gmail.com>
2021-07-28 18:50:25 -0400

    UseParams added to the file and single club query

--- react-app/src/components/SinglePostPage/SinglePostPage.js
+++ react-app/src/components/SinglePostPage/SinglePostPage.js
@@ -1,8 +1,12 @@
 import React, {useEffect} from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import styles from './singlepost.module.css';
 
 const SinglePostPage = () => {
+    const {id} = useParams();
+    const posts = useSelector(state => Object.values(state.posts))
+    const singlePost = posts.find((post) => post.id === +id)
     return (
         <div>
 

19aaf51274004d01ed52b6aefed75d5240c807ec
Machaelmus <michaeljtufo@gmail.com>
2021-07-28 00:35:26 -0400

    Figured out dropdown

--- react-app/src/components/PostContainer.js
+++ react-app/src/components/PostContainer.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { useDispatch} from 'react-redux'
-// import { useParams } from 'react-router-dom';
-// import { deleteOnePost } from '../store/posts';
+import React, {useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Home/Home.module.css';
 
 const PostContainer = ({post}) => {
-    const dispatch = useDispatch();
-    console.log('INSIDE THE COMPONENT', post)
+    const [openOptions, setOpenOptions] = useState(false);
 
-    // const deletePostFunc = () => {
-    //     dispatch(deleteOnePost(post.id))
-    // }
+    const enableOptions = () => {
+        if(openOptions) {
+            return;
+        } else {
+            setOpenOptions(true)
+        }
+    }
+
+    useEffect(() => {
+        if (!openOptions) {
+            return;
+        } else {
+            const removeOptions = () => {
+                setOpenOptions(false)
+            }
+            document.addEventListener('click', removeOptions);
+            return () => document.removeEventListener('click', removeOptions);
+        }
+    }, [setOpenOptions])
 
     return (
+
         <>
             <div className={styles.postDiv}>
+                <button onClick={enableOptions} className={styles.optionsButton}>...</button>
+            {openOptions && (
+                <div className={styles.optionsDrop}>
+                    <p>Edit</p>
+                    <p>Delete</p>
+                </div>
+            )}
                 <p className={styles.eachPostTime}>{post.timeOfPost}</p>
                 <h2 className={styles.eachPostPost}>{post.post}</h2>
             </div>

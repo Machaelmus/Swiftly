e400ab42318d01be802406324828abb6a3a42e4e
Machaelmus <michaeljtufo@gmail.com>
2021-07-28 18:55:14 -0400

    UseEffect added to the single post component

--- react-app/src/components/SinglePostPage/SinglePostPage.js
+++ react-app/src/components/SinglePostPage/SinglePostPage.js
@@ -1,17 +1,23 @@
 import React, {useEffect} from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getIndividualPost } from '../../store/posts';
 import styles from './singlepost.module.css';
 
 const SinglePostPage = () => {
+    const dispatch = useDispatch();
     const {id} = useParams();
     const posts = useSelector(state => Object.values(state.posts));
     const singlePost = posts.find((post) => post.id === +id);
-    console.log('SINGLE POST ==>', singlePost);
-    
+    console.log('SINGLE POST ==>', posts);
+
+    useEffect(() => {
+        dispatch(getIndividualPost(id))
+    })
+
     return (
         <div>
-
+            <h1>Hi</h1>
         </div>
     )
 }

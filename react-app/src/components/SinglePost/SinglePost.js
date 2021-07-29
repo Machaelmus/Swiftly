cb085d52901525c666b64a8d04f065c1bf76229b
Machaelmus <michaeljtufo@gmail.com>
2021-07-29 12:15:13 -0400

    Single post page nearly done

--- react-app/src/components/SinglePost/SinglePost.js
+++ react-app/src/components/SinglePost/SinglePost.js
@@ -2,16 +2,24 @@ import React, {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOnePost } from '../../store/posts';
-
+import styles from './singlepost.module.css';
 
 const SinglePost = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
-    
+
+    useEffect(() => {
+        dispatch(getOnePost(id))
+    }, [dispatch])
 
     return (
-        <div>
-
+        <div className={styles.singlePostPageContainer}>
+            <div className={styles.returnArrow}><h1>Return</h1></div>
+            <div className={styles.singlePostContainer}>
+                <h1>Post username</h1>
+                <p>Post time</p>
+                <p>Post itself</p>
+            </div>
         </div>
     )
 }

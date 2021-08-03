739fb233f34d7666ae4e922f5ed373c82d37917c
Machaelmus <michaeljtufo@gmail.com>
2021-08-03 15:52:55 -0400

    Userlist changes

--- react-app/src/components/UserProfile/UsersList.js
+++ react-app/src/components/UserProfile/UsersList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './userList.module.css';
 
 function UsersList() {
   const [users, setUsers] = useState([]);
@@ -16,20 +17,27 @@ function UsersList() {
   const userComponents = users.map((user) => {
     return (
 
-      <div key={user.id}>
-        <img src={user.profileImage}></img>
-        <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
-        <div>
-          <button>Follow</button>
+      <div className={styles.allusersContainer} key={user.id}>
+          <div className={styles.allusersImageandUsername}>
+            <img className={styles.allusersProfileImage} src={user.profileImage}></img>
+            <NavLink className={styles.allusersUsernames} to={`/users/${user.id}`}>{user.username}</NavLink>
+          </div>
+          <div className={styles.allusersFollowButtonContainer}>
+            <button className={styles.allusersFollowButton}>Follow</button>
+          </div>
         </div>
-      </div>
     );
   });
 
   return (
     <>
-      <h1>User List: </h1>
-      <ul>{userComponents}</ul>
+    <div className={styles.wholePageContainerForAllUsers}>
+      <h1 className={styles.usersToFollow}>Users to follow</h1>
+      <Link to="/home">
+        <h2 className={styles.returnHome}>Return</h2>
+      </Link>
+      <div className={styles.wholeBigChungus}>{userComponents}</div>
+    </div>
     </>
   );
 }

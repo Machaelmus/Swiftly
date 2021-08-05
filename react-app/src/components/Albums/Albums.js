253ee334636d47d93a6e0070afb7df96d763975a
Machaelmus <michaeljtufo@gmail.com>
2021-08-05 16:19:14 -0400

    Albums component added

---
+++ react-app/src/components/Albums/Albums.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './albums.module.css';
+
+const Albums = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Albums;

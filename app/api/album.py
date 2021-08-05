16c8c6096251db2a8a1b5edd29ab61d3c611f380
Machaelmus <michaeljtufo@gmail.com>
2021-08-05 15:54:42 -0400

    All albums route complete

--- app/api/album.py
+++ app/api/album.py
@@ -8,3 +8,8 @@ from datetime import date
 album_routes = Blueprint('albums', __name__)
 
 
+@album_routes.route('/api/albums')
+def getAlbums():
+    allAlbums = Album.query.all()
+    return {'albums': [album.to_dict() for album in allAlbums]}
+    

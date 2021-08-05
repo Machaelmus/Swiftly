2a05bf85414fe7b409f3941e6447eca632777969
Machaelmus <michaeljtufo@gmail.com>
2021-08-05 18:17:27 -0400

    Edit album thunk and reducer case complete

--- react-app/src/store/albums.js
+++ react-app/src/store/albums.js
@@ -1,5 +1,6 @@
 const GET_ALBUMS = 'albums/GET_ALBUMS';
 const CREATE_ALBUM = 'albums/CREATE_ALBUM';
+const EDIT_ALBUM = 'albums/EDIT_ALBUM';
 const DELETE_ALBUM = 'albums/DELETE_ALBUM';
 
 const getAlbums = (albums) => ({
@@ -9,6 +10,11 @@ const getAlbums = (albums) => ({
 
 const createAlbum = (album) => ({
     type: CREATE_ALBUM,
+    album,
+});
+
+const editAlbum = (album) => ({
+    type: EDIT_ALBUM,
     album,
 });
 
@@ -33,10 +39,25 @@ export const createAnAlbum = (album) => async (dispatch) => {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(album),
     })
+
     if(response.ok) {
         const createdAlbum = await response.json()
         dispatch(createAlbum(createdAlbum))
         return createdAlbum;
+    }
+}
+
+export const editAnAlbum = (id, album) => async (dispatch) => {
+    const response = await fetch(`/api/albums/${id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(album)
+    })
+
+    if(response.ok) {
+        const editedAlbum = await response.json()
+        dispatch(editAlbum(editedAlbum))
+        return editedAlbum;
     }
 }
 
@@ -45,6 +66,7 @@ export const deleteAnAlbum = (id) => async (dispatch) => {
         method: 'DELETE',
         headers: {'Content-Type': 'application/json'},
     })
+
     if(response.ok) {
         const deletedAlbum = await response.json()
         dispatch(deleteAlbum(deletedAlbum))
@@ -61,6 +83,7 @@ const albumsReducer = (state=initialState, action) => {
                 allAlbums[album.id] = album
             })
             return allAlbums;
+        case EDIT_ALBUM:
         case CREATE_ALBUM:
             return {
                 ...state,

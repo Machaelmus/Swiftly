974e105c35e7875151d7f5d592e8cfc4c05b17f9
Machaelmus <michaeljtufo@gmail.com>
2021-08-05 18:11:29 -0400

    Deleting an album complete

--- react-app/src/components/AllAlbums/AllAlbums.js
+++ react-app/src/components/AllAlbums/AllAlbums.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteAnAlbum } from '../../store/albums';
 import styles from '../Albums/albums.module.css';
 
 const AllAlbums = ({album}) => {
+    const dispatch = useDispatch();
+    const deleteOneAlbum = (e) => {
+        e.preventDefault()
+
+        dispatch(deleteAnAlbum(album.id))
+    }
 
     return (
         <div className={styles.eachAlbumBackground}>
             <h2>{album.title}</h2>
             <img src={album.coverPhotoUrl}></img>
             <p>{album.description}</p>
+            <button className={styles.eachAlbumDelete} onClick={deleteOneAlbum}>Delete Album</button>
         </div>
     )
 }

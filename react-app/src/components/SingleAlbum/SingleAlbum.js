7958c10a0f4ccb2d0722246f6e9b16ffc07e5a6f
Machaelmus <michaeljtufo@gmail.com>
2021-08-05 18:22:38 -0400

    Single album component created

---
+++ react-app/src/components/SingleAlbum/SingleAlbum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleAlbum = () => {
+    return (
+        <div>
+            <h1>Single Album Page</h1>
+        </div>
+    )
+}
+
+
+export default SingleAlbum;

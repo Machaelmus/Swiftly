94af6bdc8947c795108e4a990466b71bad525b3b
Machaelmus <michaeljtufo@gmail.com>
2021-11-23 13:24:35 -0800

    New entire albums page created

--- react-app/src/components/DiscoverAllAlbums/DiscoverAllAlbums.js
+++ react-app/src/components/DiscoverAllAlbums/DiscoverAllAlbums.js
@@ -1,12 +1,26 @@
 import React, {useEffect} from 'react';
 import { getAllAlbums } from '../../store/albums';
 import { useSelector, useDispatch } from 'react-redux';
-
+import './DiscoverAllAlbums.css';
 
 const DiscoverAllAlbums = () => {
+    const dispatch = useDispatch();
+    const albums = useSelector(state => Object.values(state.albums));
+
+    useEffect(() => {
+        dispatch(getAllAlbums());
+    }, [dispatch]);
+
     return (
-        <div>
-            
+        <div className='allAlbumsPageContainer'>
+            {albums.map((album) => (
+                <div className='allAlbumsContainer'>
+                    <h3 className='allAlbumsTitle'>{album.title}</h3>
+                    <p className='allAlbumsCreatedBy'>Album created by: <span className='allAlbumsCreatedBySpan'>{album.user.username}</span></p>
+                    <img className='allAlbumsPhoto' src={album.coverPhotoUrl}/>
+                    <p className='allAlbumsDescription'>{album.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
